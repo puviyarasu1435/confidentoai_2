d7dd53fb1f6c62324e198e8e8cfcff715825d0d2
Puviyarasu <puviyarasu5341@gmail.com>
2024-10-17 07:39:13 +0530

    ss

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.post("/sendToUnity", async (req, res) => {
         const docRef = doc(db, "System", "uZCY1O4xlKq2AOWAnm1F");
         await updateDoc(docRef, req.body);
         isChange["Environment"] = req.body.Environment;
-        io.emit("EnvData", { Index: req.body.Environment });
+       
         
         res.status(200).send("Document updated successfully");
     } catch (error) {
